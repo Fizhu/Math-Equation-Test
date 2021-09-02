a173a8783a61d193edc580f359e972d53971cf27
fizhu <hvyz.anbiya@gmail.com>
2021-09-02 14:29:25 +0700

    update mathjax and plat data

--- app/src/main/java/com/example/webviewtest/MainActivity.kt
+++ app/src/main/java/com/example/webviewtest/MainActivity.kt
@@ -9,16 +9,16 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     val htmlData =
         "<p>\$\$\\\\frac{-b \\\\pm \\\\sqrt{b^2 - 4ac}}{2a}\$\$</p>\\n<p></p>\\n<p>If you can see this equation beautifully, then the Math Equation has been installed</p>"
-
+    val htmlDataPlain = "<p>$$\\frac{-b \\pm \\sqrt{b^2 - 4ac}}{2a}$$</p>\n<p></p>\n<p>If you can see this equation beautifully, then the Math Equation has been installed</p>"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        onInit()
     }
 
     private fun onInit() {
-
+        binding.webview.text = htmlDataPlain
     }
 
 }
